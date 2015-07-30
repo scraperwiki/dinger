b116e599e7742a2d6de7e63e38bd514f906e3ad6
Dragon Dave McKee <dragon@scraperwiki.com>
2015-07-30 13:43:32 +0100

    Corrects date formatting, limits number of events in output

--- main.go
+++ main.go
@@ -38,13 +38,19 @@ func main() {
 	)
 
 	go func() {
-		for payload := range events {
-			log.Printf("Signalled via hookbot, content of payload:")
-			log.Printf("%s", payload)
+		for range events {
+			log.Printf("Recieved event.")
+
 			func() {
 				mu.Lock()
 				defer mu.Unlock()
-				eventTimes = append(eventTimes, time.Now())
+
+				eventTimes = append([]time.Time{time.Now()}, eventTimes...)
+
+				const MAX_EVENTS = 3
+				if len(eventTimes) > MAX_EVENTS {
+					eventTimes = eventTimes[:MAX_EVENTS]
+				}
 			}()
 		}
 	}()
@@ -52,7 +58,10 @@ func main() {
 	http.HandleFunc("/", func(w http.ResponseWriter, r *http.Request) {
 		mu.Lock()
 		defer mu.Unlock()
-		fmt.Fprintf(w, "<foo><bar>%v</foo></bar>\n", eventTimes)
+		for _, t := range eventTimes {
+			const RFC3339_UTC = "2006-01-02T15:04:05Z"
+			fmt.Fprintf(w, "<entry><updated>%v</updated></entry>\n", t.UTC().Format(RFC3339_UTC))
+		}
 	})
 
 	log.Fatal(http.ListenAndServe(port, nil))
